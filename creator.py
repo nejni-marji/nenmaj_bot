47439c8a98e1b55c99db6c72ce6f3cff917248c1
nejni-marji <nejni.marji@gmail.com>
2017-04-07 21:54:07 -0500

    Moved all of the usage functions into one function

--- creator.py
+++ creator.py
@@ -29,36 +29,29 @@ def bot_shutdown(bot, update):
 	sleep(1)
 	execl(dirname(__file__) + '/shutdown.py', '--')
 
-def bot_uptime(bot, update):
+def bot_usage(bot, update):
+	resp = []
+	# get ping
+	delta = datetime.now() - update.message.date
+	resp.append('Ping: ' + str(delta))
+	# get uptime
 	start = float(open(dirname(__file__) + '/private/start.txt').read())
 	date = datetime(1, 1, 1) + timedelta(seconds = time() - start)
 	now = datetime.now().strftime('%H:%M:%S')
 	days = date.day-1
 	hours = date.strftime('%H:%M:%S')
 	if days:
-		resp = '{}, up {} days, {}'.format(now, days, hours)
+		resp.append('Uptime: {}, {} days, {}'.format(now, days, hours))
 	else:
-		resp = '{}, up {}'.format(now, hours)
-	bot.send_message(update.message.chat_id,
-		resp,
-	)
-
-def bot_usage(bot, update):
+		resp.append('Uptime: {}, {}'.format(now, hours))
+	# get usage
 	cpu = cpu_percent()
 	ram = virtual_memory().percent
-	resp = [
-		'CPU: %.3f%%' % cpu,
-		'RAM: %.3f%%' % ram,
-	]
+	resp.append('CPU: %.3f%%' % cpu)
+	resp.append('RAM: %.3f%%' % ram)
+	# send message
 	bot.send_message(update.message.chat_id,
 		'\n'.join(resp),
-		reply_to_message_id = update.message.message_id,
-	)
-
-def bot_ping(bot, update):
-	delta = datetime.now() - update.message.date
-	bot.send_message(update.message.chat_id,
-		str(delta),
 		reply_to_message_id = update.message.message_id,
 	)
 
@@ -70,7 +63,5 @@ def bot_leave(bot, update):
 def main(dp):
 	dp.add_handler(tg_ext.CommandHandler('reboot', bot_reboot))
 	dp.add_handler(tg_ext.CommandHandler('shutdown', bot_shutdown))
-	dp.add_handler(tg_ext.CommandHandler('uptime', bot_uptime))
 	dp.add_handler(tg_ext.CommandHandler('usage', bot_usage))
-	dp.add_handler(tg_ext.CommandHandler('ping', bot_ping))
 	dp.add_handler(tg_ext.CommandHandler('leave', bot_leave))
