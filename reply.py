449d769d4281244278fdcbf89aff1de2398dad38
nejni-marji <nejni.marji@gmail.com>
2017-07-06 17:23:50 -0500

    Stopped sending reply and alert notifications for private messages, now forwards all private messages

--- reply.py
+++ reply.py
@@ -24,53 +24,63 @@ def reply_check(bot, update):
 		alert = (True in (i in msg.text.lower() for i in alert_list))
 	except AttributeError:
 		alert = False
+	# check if private message
+	private = msg.chat_id >  0
 	# kill undesireables
-	if not reply and not alert: return None
 
-	# get message info
-	chat = update.message.chat.id
-	try:
-		user = update.from_user
-	except AttributeError:
-		user = update.message.from_user
-	member = bot.get_chat_member(
-		chat, user.id
-	)
+	# define functions
+	def send_notification():
+		# get message info
+		chat = update.message.chat.id
+		try:
+			user = update.from_user
+		except AttributeError:
+			user = update.message.from_user
+		member = bot.get_chat_member(
+			chat, user.id
+		)
 
-	# put message info into list
-	resp = []
-	# chat
-	resp.append(str(msg.date))
-	if msg.chat.title: resp.append(str(msg.chat.title) + ':')
-	else: resp.append(':')
-	resp.append(str(msg.chat.id))
-	# user
-	resp.append(str(user.id))
-	if user.username:
-		resp[3] += ' (@%s)' % user.username
-	resp.append('P-nomo: ' + user.first_name)
-	if user.last_name:
-		resp.append('F-nomo: ' + user.last_name)
-	resp.append('Stato: %s' % member.status)
+		# put message info into list
+		resp = []
+		# chat
+		resp.append(str(msg.date))
+		if msg.chat.title: resp.append(str(msg.chat.title) + ':')
+		else: resp.append(':')
+		resp.append(str(msg.chat.id))
+		# user
+		resp.append(str(user.id))
+		if user.username:
+			resp[3] += ' (@%s)' % user.username
+		resp.append('P-nomo: ' + user.first_name)
+		if user.last_name:
+			resp.append('F-nomo: ' + user.last_name)
+		resp.append('Stato: %s' % member.status)
 
-	# send alert
-	bot.send_message(
-		chat_id = myself,
-		text = '\n'.join(resp)
-	)
+		# send alert
+		bot.send_message(
+			chat_id = myself,
+			text = '\n'.join(resp)
+		)
 
-	if reply:
+		if reply:
+			bot.forward_message(
+				chat_id = myself,
+				from_chat_id = msg.chat.id,
+				message_id = msg.reply_to_message.message_id
+			)
+
+	def send_forward():
 		bot.forward_message(
 			chat_id = myself,
 			from_chat_id = msg.chat.id,
-			message_id = msg.reply_to_message.message_id
+			message_id = msg.message_id
 		)
 
-	bot.forward_message(
-		chat_id = myself,
-		from_chat_id = msg.chat.id,
-		message_id = msg.message_id
-	)
+	if private:
+		send_forward()
+	elif reply or alert:
+		send_notification()
+		send_forward()
 
 def main(dp):
 	dp.add_handler(tg_ext.MessageHandler(tg_ext.Filters.all, reply_check), group = 1)
