0255b4f56f53bd2582c9b1013038faed23e3e516
nejni-marji <nejni.marji@gmail.com>
2017-08-03 18:15:50 -0500

    Finally fixed 'id' triggering the Ido text

--- text_parse.py
+++ text_parse.py
@@ -269,7 +269,7 @@ def text_parse(bot, update):
 				"Ja, fek al vi!",
 			)
 			bot_resp(
-				"id((ists?)?(?<!s)oj?)?)",
+				"id(ist(s|(?<!s)(oj?n?)?)|o)",
 				"Fekacho a vu!",
 			)
 		if check_at_bot():
